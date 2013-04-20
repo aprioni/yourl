05c909edbc14a36d205c704667e95cf3b5a7108a
Alessandro Prioni <alessandroprioni@gmail.com>
2013-04-20 12:54:09 +0200

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Yourl::Application.routes.draw do
   
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signup', to: 'users#new'
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   root to: "pages#home"
-
-  match 'about', to: 'pages#about'
+  match '/about', to: 'pages#about'
 
 
   # The priority is based upon order of creation:
